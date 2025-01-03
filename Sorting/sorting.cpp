6acaadad2a6cba132f47c5cb2bfe2ea23156dc4e
Honsage <musatov.i.a@edu.mirea.ru>
2025-01-04 01:14:41 +0300

    feat(sorting): add implementation of cocktail sorting in ascending order

--- Sorting/sorting.cpp
+++ Sorting/sorting.cpp
@@ -2,7 +2,7 @@
 #define uint unsigned int
 
 template <typename T>
-void bubbleSort(T arr[], int size) {
+void bubbleSort(T arr[], uint size) {
     bool isSorted = false;
     for (uint iter = 1; iter < size && !isSorted; ++iter) {
         isSorted = true;
@@ -16,7 +16,7 @@ void bubbleSort(T arr[], int size) {
 }
 
 template <typename T>
-void selectionSort(T arr[], int size) {
+void selectionSort(T arr[], uint size) {
     uint idxOfMin;
     for (uint i = 0; i < size-1; ++i) {
         idxOfMin = i;
@@ -30,7 +30,7 @@ void selectionSort(T arr[], int size) {
 }
 
 template <typename T>
-void insertionSort(T arr[], int size) {
+void insertionSort(T arr[], uint size) {
     T key;
     int j;
     for (uint i = 1; i < size; ++i) {
@@ -41,5 +41,32 @@ void insertionSort(T arr[], int size) {
             --j;
         }
         arr[j+1] = key;
+    }
+}
+
+template <typename T>
+void cocktailSort(T arr[], uint size) {
+    bool isSwapped = true;
+    uint start = 0;
+    uint end = size - 1;
+    while (isSwapped) {
+        std::cout << start << " " << end << "\n";
+        isSwapped = false;
+        for (uint i = start; i < end; ++i) {
+            if (arr[i] > arr[i+1]) {
+                std::swap(arr[i], arr[i+1]);
+                isSwapped = true;
+            }
+        }
+        if (!isSwapped) break;
+        --end;
+
+        for (uint i = end - 1; (int)i >= (int)start; --i) {
+            if (arr[i] > arr[i+1]) {
+                std::swap(arr[i], arr[i+1]);
+                isSwapped = true;
+            }
+        }
+        ++start;
     }
 }
