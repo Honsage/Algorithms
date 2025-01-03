80d673c6b0fd510a1ce8c96ea014a41bfeb4d58f
Honsage <musatov.i.a@edu.mirea.ru>
2025-01-04 00:24:55 +0300

    feat(sorting): add implementation of insertion sorting in ascending order

--- Sorting/sorting.cpp
+++ Sorting/sorting.cpp
@@ -18,13 +18,28 @@ void bubbleSort(T arr[], int size) {
 template <typename T>
 void selectionSort(T arr[], int size) {
     uint idxOfMin;
-    for(uint i = 0; i < size-1; ++i) {
+    for (uint i = 0; i < size-1; ++i) {
         idxOfMin = i;
-        for(uint k = i + 1; k < size; ++k) {
-            if(arr[k] < arr[idxOfMin]) {
+        for (uint k = i + 1; k < size; ++k) {
+            if (arr[k] < arr[idxOfMin]) {
                 idxOfMin = k;
             }
         }
         std::swap(arr[i], arr[idxOfMin]);
+    }
+}
+
+template <typename T>
+void insertionSort(T arr[], int size) {
+    T key;
+    int j;
+    for (uint i = 1; i < size; ++i) {
+        key = arr[i];
+        j = i-1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j+1] = arr[j];
+            --j;
+        }
+        arr[j+1] = key;
     }
 }
