64bac93391064aac92944646090e3878d78b5e87
Honsage <musatov.i.a@edu.mirea.ru>
2025-01-27 21:22:11 +0300

    feat(compression): add implementation of Huffman compression algorithm

--- Compression/huffman.cpp
+++ Compression/huffman.cpp
@@ -1,1 +1,84 @@
+#include <queue>
+
 #include "huffman.h"
+
+#include <iostream>
+
+HuffmanCompressor::HuffmanCompressor() {}
+
+void HuffmanCompressor::encode(const std::string& text) {
+    this->reset();
+
+    std::unordered_map<char, int> frequency_table;
+    for (char c : text) {
+        ++frequency_table[c];
+    }
+
+    std::priority_queue<Node*, std::vector<Node*>, comp> p_queue;
+
+    for (auto record : frequency_table) {
+        p_queue.push(new Node(record.first, record.second, nullptr, nullptr));
+    }
+
+    Node* left;
+    Node* right;
+    while (p_queue.size() != 1) {
+        left = p_queue.top();
+        p_queue.pop();
+        right = p_queue.top();
+        p_queue.pop();
+
+        unsigned int common_freq = left->freq + right->freq;
+        p_queue.push(new Node('\0', common_freq, left, right));
+    }
+
+    this->root = p_queue.top();
+    this->recursive_encoding(this->root);
+    // huffman codes are computed
+
+    for (char c : text) {
+        this->m_compressed_text += this->m_huffman_codes[c];
+    }
+}
+
+std::string HuffmanCompressor::decode() {
+    int index = -1;
+    const int encoded_size = static_cast<int>(this->m_compressed_text.size());
+    while (encoded_size != 0 && index < encoded_size) {
+        this->recursive_decoding(this->root, index);
+    }
+    return "";
+}
+
+void HuffmanCompressor::reset() {
+    this->m_huffman_codes.clear();
+    this->m_compressed_text = "";
+    this->root = nullptr;
+}
+
+void HuffmanCompressor::recursive_encoding(Node* root, std::string code) {
+    if (root == nullptr) {
+        return;
+    }
+    if (! root->left && ! root->right) {
+        this->m_huffman_codes[root->c] = code;
+    }
+    this->recursive_encoding(root->left, code + "0");
+    this->recursive_encoding(root->right, code + "1");
+}
+
+void HuffmanCompressor::recursive_decoding(Node *root, int &index) {
+    if (root == nullptr) {
+        return;
+    }
+    if (! root->left && ! root->right) {
+        std::cout << root->c;
+        return;
+    }
+    index++;
+    if (this->m_compressed_text[index] == '0') {
+        this->recursive_decoding(root->left, index);
+    } else {
+        this->recursive_decoding(root->right, index);
+    }
+}
--- Compression/huffman.h
+++ Compression/huffman.h
@@ -1,10 +1,40 @@
 #ifndef HUFFMAN_H
 #define HUFFMAN_H
 
-
+#include <string>
+#include <unordered_map>
 
 class HuffmanCompressor {
+public:
+    HuffmanCompressor();
 
+    void encode(const std::string& text);
+    std::string decode();
+
+    void reset();
+private:
+    struct Node {
+        char c;
+        unsigned int freq;
+        Node* left;
+        Node* right;
+        Node(char c, unsigned int freq, Node* left, Node* right) :
+            c(c), freq(freq), left(left), right(right) {}
+    };
+
+    struct comp {
+        bool operator()(const Node* l, const Node* r) const {
+            return l->freq > r->freq;
+        }
+    };
+
+    std::unordered_map<char, std::string> m_huffman_codes;
+
+    std::string m_compressed_text;
+    Node* root;
+
+    void recursive_encoding(Node* root, std::string code = "");
+    void recursive_decoding(Node* root, int& index);
 };
 
 
